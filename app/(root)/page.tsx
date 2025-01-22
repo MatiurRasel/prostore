6d0d753d86eb5a3185eb6b69e455835ab00c92a3
Matiur Rasel <matiurrasel1002@gmail.com>
2025-01-22 21:55:09 +0600

    add initial

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { resolve } from "path";
+
+const Homepage = () => {
+
+  return ( <>
+    <h1>Homepage</h1>
+  </> );
+}
+ 
+export default Homepage;
