2c7e5a582399dde80b438611bec288315db9fc9a
Matiur Rasel <matiurrasel1002@gmail.com>
2025-01-23 15:18:34 +0600

    added-changes

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,7 +1,11 @@
 import ProductCard from "./product-card";
+import { Product } from "@/types";
 
-
-const ProductList = ({data,title,limit} : {data: any; title?:string; limit?: number; }) => {
+const ProductList = ({data,title,limit} : {
+        data: Product[]; 
+        title?:string; 
+        limit?: number; 
+    }) => {
     const limitedData = limit ? data.slice(0,limit) : data;
     return (
         <div className="my-10">
@@ -9,7 +13,7 @@ const ProductList = ({data,title,limit} : {data: any; title?:string; limit?: num
             {data.length > 0 ? (
                 <div 
                     className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {limitedData.map((product: any) =>(
+                    {limitedData.map((product: Product) =>(
                        <ProductCard key={product.slug} product={product}/>
                     ))}
                 </div>
