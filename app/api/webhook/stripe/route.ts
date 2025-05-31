217e9e0a662d4251c38e391a7fac083a8ba550e1
Matiur Rasel <matiurrasel1002@gmail.com>
2025-05-31 13:55:59 +0600

    webhook stripe added

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,41 @@
+import { updateOrderToPaid } from "@/lib/actions/order.actions";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: NextRequest) {
+    //Build the webhook event
+    const event =await Stripe.webhooks.constructEventAsync(
+        await request.text(),
+        request.headers.get('stripe-signature')!,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+
+    //Check For Successful Payment
+    if(event.type === 'charge.succeeded') {
+        const {object} = event.data;
+
+        //Update the order to paid
+        await updateOrderToPaid({
+            orderId: object.metadata.orderId,
+            paymentResult: {
+                id: object.id,
+                status: "COMPLETED",
+                email_address: object.billing_details.email!,
+                pricePaid: (object.amount / 100).toFixed(),
+            }
+                
+        });
+
+        return NextResponse.json({
+            message: 'Update Order to Paid was successful',
+
+        }, {status: 200});
+    }
+
+    return NextResponse.json({
+        message: 'event is not a charge.succeeded event',
+    }, {status: 400});
+}
+
